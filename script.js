106686a75df9351e57730432d954023e85e6a8bc
Hellibell <133568395+Hellibell@users.noreply.github.com>
2025-08-25 04:25:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-// ตัวอย่างถ้าต้องการแจ้ง alert เวลาคลิก
-const links = document.querySelectorAll(".links a");
-
-links.forEach(link => {
-  link.addEventListener("click", () => {
-    console.log(`Opening ${link.href}`);
-  });
-});
+console.log("Bio Page Loaded!");
