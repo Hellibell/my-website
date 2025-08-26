939126ed7d3a6ff92720a5ca2252a7e2fc2e2a7d
Hellibell <133568395+Hellibell@users.noreply.github.com>
2025-08-27 03:11:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// Log ข้อความตอนโหลดหน้า
 console.log("Bio Page Loaded!");
+
+// ตรวจสอบว่าเคยเข้ามาแล้วหรือยัง
+if (!localStorage.getItem("visited")) {
+  // ถ้ายังไม่เคยเข้ามา -> นับเป็น 1
+  localStorage.setItem("visited", "true");
+  localStorage.setItem("siteViews", 1);
+}
+
+// ดึงค่าผู้เข้าชม
+let views = localStorage.getItem("siteViews") || 1;
+
+// แสดงผลใน view-counter พร้อมใส่รูป eye.png แทน emoji
+document.querySelector(".view-counter").innerHTML =
+  '<img src="assets/Photo/eye.png" alt="eye" width="16" height="16" style="vertical-align:middle;"> ' 
+  + views + " เข้าชม";
